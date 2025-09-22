f8ac07fe2b72b2758aa80af8fdf1ccb3d86b4cea
Sabnei <sebasplay417@gmail.com>
2025-09-22 13:49:03 -0600

    Refactor MongoDB environment variable handling and add health check endpoint

--- backend/routes.py
+++ backend/routes.py
@@ -16,15 +16,15 @@ songs_list: list = json.load(open(json_url))
 
 # client = MongoClient(
 #     f"mongodb://{app.config['MONGO_USERNAME']}:{app.config['MONGO_PASSWORD']}@localhost")
-mongodb_service = os.environ.get('MONGODB_SERVICE')
-mongodb_username = os.environ.get('MONGODB_USERNAME')
-mongodb_password = os.environ.get('MONGODB_PASSWORD')
-mongodb_port = os.environ.get('MONGODB_PORT')
+mongodb_service = os.environ.get("MONGODB_SERVICE")
+mongodb_username = os.environ.get("MONGODB_USERNAME")
+mongodb_password = os.environ.get("MONGODB_PASSWORD")
+mongodb_port = os.environ.get("MONGODB_PORT")
 
-print(f'The value of MONGODB_SERVICE is: {mongodb_service}')
+print(f"The value of MONGODB_SERVICE is: {mongodb_service}")
 
 if mongodb_service == None:
-    app.logger.error('Missing MongoDB server in the MONGODB_SERVICE variable')
+    app.logger.error("Missing MongoDB server in the MONGODB_SERVICE variable")
     # abort(500, 'Missing MongoDB server in the MONGODB_SERVICE variable')
     sys.exit(1)
 
@@ -45,9 +45,14 @@ db = client.songs
 db.songs.drop()
 db.songs.insert_many(songs_list)
 
+
 def parse_json(data):
     return json.loads(json_util.dumps(data))
+
 
 ######################################################################
 # INSERT CODE HERE
 ######################################################################
+@app.route("/health")
+def healthz():
+    return jsonify(status="OK"), 200
